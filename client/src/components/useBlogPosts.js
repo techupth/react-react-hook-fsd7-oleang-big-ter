1b0ceecc2e31658d350db6c9c82096df410e17aa
Sirawit-Boon <ter.sirawit1997@gmail.com>
2024-10-31 14:17:39 +0700

    feat: add useBlogpost for use custom hook

---
+++ client/src/components/useBlogPosts.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+function useBlogPosts() {
+  const [posts, setPosts] = useState([]);
+  const [isError, setIsError] = useState(null);
+  const [isLoading, setIsLoading] = useState(null);
+
+  const getPosts = async () => {
+    try {
+      setIsError(false);
+      setIsLoading(true);
+      const results = await axios("http://localhost:4000/posts");
+      console.log(results.data)
+      setPosts(results.data.data);
+      setIsLoading(false);
+    } catch (error) {
+      setIsError(true);
+    }
+  };
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+  return {posts, isError, isLoading}
+}
+
+export default useBlogPosts;
